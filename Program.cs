dac31e46559b6a7a23e6c4f0af7b2ccc2b88e456
leeool <lgonsalez02@gmail.com>
2022-03-18 22:40:35 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int n1, n2, resultado;
+
+Console.Write("Digite um numero: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite outro numero: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+resultado = n1 + n2;
+
+Console.WriteLine($"{resultado}");
